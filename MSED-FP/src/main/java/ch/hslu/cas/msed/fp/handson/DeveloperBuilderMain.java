9a1136ac75c75eb396e490cdafb1710bb08a13b5
Jürg Nietlispach <juerg.nietlispach@hslu.ch>
2023-11-02 16:01:25 +0100

    WIP

--- MSED-FP/src/main/java/ch/hslu/cas/msed/fp/handson/DeveloperBuilderMain.java
+++ MSED-FP/src/main/java/ch/hslu/cas/msed/fp/handson/DeveloperBuilderMain.java
@@ -7,25 +7,41 @@ public class DeveloperBuilderMain
     public static void main(String[] args)
     {
         /*
-        * Excercise / Todo:
+        * 1) Excercise / Todo:
             Developer-Klasse erweitern:
             Neue Developer-Felder: Salary, #Cups of Coffee per Day
             Berechnung von Average Salary und Total Cups of Coffee
         */
 
-        // 2) Stream-API
+        // 2) Todo: Neue Felder hier ergänzen
         List<Developer> developers = List.of(new Developer("Dan", "Stravinski", "dan@google.com"),
                 new Developer("Samantha", "Jones", "sj@dev.to"),
                 new Developer("Ray", "Brown", "music@jazznight.info")
         );
-        // Using Lambdas
+
+        // Get Dan!
+        for(var dev : developers)
+        {
+            if(dev.Firstname.equals("Dan"))
+            {
+                System.out.println(dev.Firstname + " " + dev.Lastname + " " + dev.Email);
+            }
+        }
+
+        // Todo: Rewrite Using Stream / Filter
+
+        /* Solution:
         Developer dev = developers.stream().filter(x -> x.Firstname.equals("Dan")).findFirst().get();
-        System.out.println(dev.Firstname + " " + dev.Lastname + " " + dev.Email);
+        System.out.println(dev.Firstname + " " + dev.Lastname + " " + dev.Email);*/
 
         // 3) Fluent API, DSLs, ...
+        // Todo: Rewrite the creation of the developer list using the Developer Builder Class
+
+        /* Solution for single developer:
         DeveloperBuilder builder = new DeveloperBuilder();
         Developer buildDev = builder.FirstName("Dan").LastName("Stravinski").Email("dan@google.com").build();
         System.out.println(buildDev.Firstname + " " + buildDev.Lastname + " " + buildDev.Email);
+        */
 
         // 4) Custom DSLs, ...
         DeveloperQL ql = new DeveloperQL(developers);
@@ -34,5 +50,10 @@ public class DeveloperBuilderMain
                 .Fetch();
 
         results.forEach(System.out::println);
+
+        /*
+            Excercise / Todo:
+            Implement Insert, Update, Order
+        */
     }
 }
